3d084ad1c2bdc2509611489e6de810a5a72f2511
Eduard Valeyev <eduard@valeyev.net>
2025-01-29 08:04:24 -0500

    Update small_vector.h
    NAMESPACE_{BEGIN,END} -> PYBIND11_NAMESPACE_{BEGIN,END}

--- include/pybind11/boost/container/small_vector.h
+++ include/pybind11/boost/container/small_vector.h
@@ -3,12 +3,12 @@
 #include "../../stl.h"
 #include <boost/container/small_vector.hpp>
 
-NAMESPACE_BEGIN(PYBIND11_NAMESPACE)
-NAMESPACE_BEGIN(detail)
+PYBIND11_NAMESPACE_BEGIN(PYBIND11_NAMESPACE)
+PYBIND11_NAMESPACE_BEGIN(detail)
 
   template <typename T, std::size_t N>
   struct type_caster< boost::container::small_vector<T,N> >
     : list_caster< boost::container::small_vector<T,N>, T > { };
 
-NAMESPACE_END(detail)
-NAMESPACE_END(PYBIND11_NAMESPACE)
+PYBIND11_NAMESPACE_END(detail)
+PYBIND11_NAMESPACE_END(PYBIND11_NAMESPACE)
